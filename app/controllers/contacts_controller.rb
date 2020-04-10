b4e3cdb01db69307c3cbd1e670aab28cdbd5e3da
iamtamtamiam <vutam_a@hotmail.com>
2020-04-10 00:16:12 -0500

    add contact create and show

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,28 @@
 class ContactsController < ApplicationController
+
+    def new
+        @contact = Contact.new
+    end 
+
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to contact_path(@contact)
+        else
+            render new_contact_path
+        end 
+
+    end 
+
+    def show
+        @contact = Contact.find_by(id: params[:id])
+        redirect_to new_contact_path if !@contact
+    end 
+
+    private
+
+    def contact_params
+        params.require(:contact).permit(:name, :description, :notes)
+    end 
+
 end
