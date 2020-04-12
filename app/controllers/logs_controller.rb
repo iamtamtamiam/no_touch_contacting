2ee139d301f7ef8c0fe3c15e8d3856b6bfb98752
iamtamtamiam <vutam_a@hotmail.com>
2020-04-12 11:48:23 -0500

    add log edit and update

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -24,6 +24,27 @@ class LogsController < ApplicationController
         @log = Log.find_by(id: params[:id])
     end 
 
+    def edit
+        @log = Log.find_by(id: params[:id])
+        @contacts = Contact.all
+        redirect_to logs_path if !@log || @log.employee != current_employee
+        #add flash message for this log doesnt exist
+        #only user who created log can edit it. 
+        # maybe add last upadated by? on the show page
+    end 
+
+    def update
+        @log = Log.find_by(id: params[:id])
+        redirect_to logs_path if !@log
+        if @log.update(log_params)
+            redirect_to log_path(@log)
+        else
+            render :edit
+            #add flash message
+        end 
+    end 
+
+
 
 
 
