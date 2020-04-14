ea8eeaf66a4e45303b558cf77c8189008c47bd84
iamtamtamiam <vutam_a@hotmail.com>
2020-04-13 21:12:48 -0500

    add date validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,7 @@ class Contact < ApplicationRecord
     has_many :logs
     has_many :employees, through: :logs
     
+    validates :name, presence: true 
 
     scope :alpha, -> { order(:name) }
 
--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,6 @@ class Log < ApplicationRecord
     belongs_to :employee
     belongs_to :contact
     
+
+    validates :date, presence: true
 end
